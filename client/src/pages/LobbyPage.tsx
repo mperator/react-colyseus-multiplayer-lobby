51deb03f19d25f6cbcc859d085497b12bc076171
mperator <develop@mperia.de>
2022-03-23 21:38:24 +0100

    Add lobby room handling and room handling.

--- client/src/pages/LobbyPage.tsx
+++ client/src/pages/LobbyPage.tsx
@@ -1,12 +1,63 @@
-import type { Client } from "colyseus.js"
+import { useCallback, useEffect, useState } from "react"
+import type { Client, Room, RoomAvailable } from "colyseus.js"
+import { Link } from "react-router-dom"
 
 type Props = {
     client: Client
 }
 
-const LobbyPage = (props: Props) => {
+const LobbyPage = ({ client }: Props) => {
+    const [rooms, setRooms] = useState<RoomAvailable[]>([])
+
+    const handleLobbyRoomsInit = useCallback((r: RoomAvailable[]) => setRooms(r), [rooms]);
+    const handleLobbyRoomsAdd = useCallback(([id, room]: any) => {
+        setRooms(r => {
+            const index = r.findIndex(x => x.roomId === id);
+            if(index !== -1) {
+                r[index] = room;
+                return [...r];
+            }
+            return [...r, room];
+        });
+    }, [rooms]);
+    const handleLobbyRoomsRemove = useCallback(id => setRooms(r => r.filter(x => x.roomId !== id)), [rooms]);
+
+    useEffect(() => {
+        let lobby: Room;
+        (async () => {
+            lobby = await client.joinOrCreate('lobby');
+            lobby.onMessage("rooms", handleLobbyRoomsInit)
+            lobby.onMessage("+", handleLobbyRoomsAdd)
+            lobby.onMessage("-", handleLobbyRoomsRemove);
+        })().catch(e => console.error(e));
+
+      return () => {
+            lobby.leave(true);
+      }
+    }, []);
+
+    const handleJoinOrCreateTestRoomAsync = async () => {
+        await client.joinOrCreate("testroom");
+    }
+
     return (
-        <div>LobbyPage</div>
+        <div>
+            <h1>Lobby</h1>
+            <input type="button" value="create and join" onClick={handleJoinOrCreateTestRoomAsync}/>
+
+            <div>
+                {rooms.map(r => (
+                    <div key={r.roomId}>
+                        <span>{r.roomId} {r.clients} {r.maxClients}</span>
+
+                        {r.clients < r.maxClients ? 
+                            <Link to={`/${r.roomId}`}>join</Link> :
+                            null
+                        }
+                    </div>
+                ))}
+            </div>
+        </div>
     )
 }
 
--- client/src/pages/RoomPage.tsx
+++ client/src/pages/RoomPage.tsx
@@ -1,15 +1,44 @@
-import { Client } from 'colyseus.js';
+import { Client, Room } from 'colyseus.js';
 import { useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 
 type Props = {
     client: Client
 }
 
-const RoomPage = (props: Props) => {
+const RoomPage = ({ client }: Props) => {
     const { roomId } = useParams();
 
+    const [session, setSession] = useState("");
+
+    useEffect(() => {
+        let room: Room;
+        
+        (async (roomId: any) => {
+            const item = localStorage.getItem(roomId);
+    
+            if (item) {
+                const { roomId, sessionId } = JSON.parse(item);
+                room = await client.reconnect(roomId, sessionId);
+                localStorage.setItem(roomId, JSON.stringify({ roomId: room.id, sessionId: room.sessionId }));
+            } else {
+                room = await client.joinById(roomId);
+                localStorage.setItem(roomId, JSON.stringify({ roomId: room.id, sessionId: room.sessionId }));
+            }
+    
+            setSession(room.sessionId);
+        })(roomId);
+
+        return () => {
+            room.leave(false);
+        }
+    }, []);
+
     return (
-        <div>RoomPage {roomId}</div>
+        <div>
+            <h1>RoomPage {roomId}</h1>
+            <span>{session}</span>
+        </div>
     )
 }
 
